013d5d137d74fa632c3ea64b932b9f46904bb7f7
Kaizen-Mongolia <kaizen-mongolia@bulgantamirs-mac-mini.local>
2016-01-14 18:10:37 +0800

    Initial Commit

---
+++ Minu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Minu
+//
+//  Created by Kaizen-Mongolia on 1/14/16.
+//  Copyright © 2016 Kaizen-Mongolia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Minu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Minu
+//
+//  Created by Kaizen-Mongolia on 1/14/16.
+//  Copyright © 2016 Kaizen-Mongolia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
